1a555373eb84131dcf202f67e7367888872c75b4
fernandogmar <fernandogmar@users.noreply.github.com>
2023-11-03 14:43:04 +0100

    there is no need to set logger and plugin name mappers, they are set now by default

--- packages/store/src/configuration.js
+++ packages/store/src/configuration.js
@@ -1,17 +1,11 @@
 import DIALOG from 'dialog/register.js';
 import MENU from 'menu/register.js';
-import {
+import reduxPluginCreatorStateConfigurator, {
     //MANY_GROUPS_TO_MANY_PLUGINS,
     //MANY_GROUPS_TO_ONE_PLUGIN,
-    ONE_GROUP_TO_ONE_PLUGIN,
     ONE_GROUP_TO_MANY_PLUGINS,
-    getPluginNames,
-    getLoggerNames,
-    //registerPluginAsLogger
-} from 'redux-plugin-creator';
-import reduxPluginCreatorStateConfigurator, {
+    ONE_GROUP_TO_ONE_PLUGIN,
     configureDefaultPluginRelationship,
-    configureNameMappers,
     configurePluginRelationship
 } from 'redux-plugin-creator/state.configurator.js';
 
@@ -19,7 +13,6 @@ const getConfiguration = (initial_state) => reduxPluginCreatorStateConfigurator(
     .map(configureDefaultPluginRelationship(ONE_GROUP_TO_ONE_PLUGIN))
     .map(configurePluginRelationship(DIALOG, ONE_GROUP_TO_MANY_PLUGINS))
     .map(configurePluginRelationship(MENU, ONE_GROUP_TO_MANY_PLUGINS))
-    .map(configureNameMappers(getLoggerNames(), getPluginNames()))
     .get();
 
 
